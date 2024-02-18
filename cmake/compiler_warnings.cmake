bd7945aae67c7291787d1f787d23c1ee8eb4f0a1
Volodymyr Sachko <volodymyrsachko1@gmail.com>
2024-02-18 16:28:47 +0200

    implemented basic cmake lists file

---
+++ cmake/compiler_warnings.cmake
@@ -0,0 +1,21 @@
+set(COMPILER_WARNINGS 
+    -Wall
+    -Wextra
+    -Wshadow
+    -Wnon-virtual-dtor
+    -Wold-style-cast
+    -Wcast-align
+    -Wunused
+    -Woverloaded-virtual
+    -Wpedantic
+    -Wconversion
+    -Wsign-conversion
+    -Wnull-dereference
+    -Wdouble-promotion
+    -Wformat=2
+    -Wimplicit-fallthrough
+    -Wmisleading-indentation
+    -Wduplicated-cond
+    -Wduplicated-branches
+    -Wlogical-op
+    -Wuseless-cast)
